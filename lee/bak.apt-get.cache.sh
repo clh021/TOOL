97a7a485f2b652d25a3023f2f96e7604efe29892
chenlianghong <clh021@gmail.com>
2015-03-06 16:20:20 +0800

    Finishing shells

--- lee/bak.apt-get.cache.sh
+++ lee/bak.apt-get.cache.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "$(date +%Y-%m-%d_%H:%M:%S) $0" >> ~/WORK/cron.result.txt;
 # 打印拷贝的所有文件，不要覆盖已存在的文件
-cp -vn /var/cache/apt/archives/* ~/SOFT/apt-get.cache/  | sed '/lock"$/d' >> ~/WORK/cron.result.txt
+cp -vn /var/cache/apt/archives/* ~/下载/apt-get.cache/  | sed '/lock"$/d' >> ~/WORK/cron.result.txt
+
